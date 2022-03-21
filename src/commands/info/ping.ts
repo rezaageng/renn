8dd83ea67b424de0f400acdb69bab8b1c65c564a
Reza Ageng <rezaageng5@gmail.com>
2022-03-21 13:42:51 +0700

    add embed

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -1,9 +1,18 @@
+import { MessageEmbed } from "discord.js"
 import { Command } from "../../structures/Command"
 
 export default new Command({
   name: "ping",
-  description: "Pong!",
-  run: async ({ interaction }) => {
-    interaction.reply("Pong!")
+  description: "Ping renn!",
+  run: async ({ interaction, client }) => {
+    const pingEmbed = new MessageEmbed()
+      .setColor("#712B75")
+      .setTitle("Pong")
+
+      .addField("Latency", `🏓 ${Date.now() - interaction.createdTimestamp}ms`)
+      .addField("API Latency", `🔥 ${Math.round(client.ws.ping)}ms`)
+      .setTimestamp()
+
+    return interaction.reply({ embeds: [pingEmbed] })
   },
 })
