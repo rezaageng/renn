1283e55f0ced080e881206583300c83f77172f39
Reza Ageng <rezaageng5@gmail.com>
2022-03-21 11:19:21 +0700

    deleted unused file

--- src/@types/discord.d.ts
+++
@@ -1,5 +0,0 @@
-// declare module "discord.js" {
-//   export interface Client {
-//     commands: Collection<unknown, string>
-//   }
-// }
--- src/utils/functions.ts
+++
@@ -1,7 +0,0 @@
-import fs from "fs"
-
-const getFiles = (path: string, ending: string) => {
-  return fs.readdirSync(path).filter((file) => file.endsWith(ending))
-}
-
-module.exports = { getFiles }
