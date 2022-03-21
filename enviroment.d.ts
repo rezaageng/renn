1579b0049934daadf111393807baed17d41e94c8
Reza Ageng <rezaageng5@gmail.com>
2022-03-21 14:17:06 +0700

    update dev role & admin id

--- enviroment.d.ts
+++ enviroment.d.ts
@@ -2,7 +2,8 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       TOKEN: string
-      DEV_ID: string
+      DEV_ROLE_ID: string
+      ADMIN_ID: string
       GUILD_ID: string
       ENVIROMENT: string
     }
