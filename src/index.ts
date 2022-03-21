51a6529771890081a140e7c9f24e84113f1e5cb9
Reza Ageng <rezaageng5@gmail.com>
2022-03-21 11:48:41 +0700

    start bot

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,7 @@
-import { Client, Collection } from "discord.js"
-import "dotenv/config"
-import config from "./config"
+import { ExtendedClient } from "./structures/Client"
 
-const client: any = new Client({
-  partials: ["CHANNEL"],
-  intents: [
-    "GUILDS",
-    "GUILD_MESSAGES",
-    "GUILD_MEMBERS",
-    "GUILD_VOICE_STATES",
-    "DIRECT_MESSAGES",
-  ],
-})
+require("dotenv").config()
 
-let bot = {
-  client,
-  dev: config.DEV,
-}
+export const client = new ExtendedClient()
 
-client.commands = new Collection()
-client.on("ready", () => {
-  console.log(`${client.user?.tag} is online!`)
-  client.user?.setActivity("Your Voice", { type: "LISTENING" })
-})
-
-client.login(config.TOKEN)
+client.start()
