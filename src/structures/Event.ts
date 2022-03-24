2ece4622552ecd03960b264a528b7d3671d4e520
Reza Ageng <rezaageng5@gmail.com>
2022-03-24 11:52:56 +0700

    avoid any

--- src/structures/Event.ts
+++ src/structures/Event.ts
@@ -3,7 +3,6 @@ import { ClientEvents } from "discord.js"
 export class Event<Key extends keyof ClientEvents> {
   constructor(
     public event: Key,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    public run: (...args: ClientEvents[Key]) => any
+    public run: (...args: ClientEvents[Key]) => void
   ) {}
 }
