45321e914d36ded08e625094f1baba4b172f424c
Reza Ageng <rezaageng5@gmail.com>
2022-03-27 11:59:55 +0700

    update activity

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,6 +2,8 @@ import chalk from "chalk"
 import { Event } from "../structures/Event"
 
 export default new Event("ready", (client) => {
-  client.user.setActivity("With You", { type: "PLAYING" })
+  const memberCount = client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)
+
+  client.user.setActivity(`With ${memberCount} others`, { type: "PLAYING" })
   console.log(chalk.bgGreen.black(`${client.user.tag} is alive!`))
 })
