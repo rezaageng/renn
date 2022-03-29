d177ec3087510f090619d8820f5a639ce22b38f3
Reza Ageng <rezaageng5@gmail.com>
2022-03-29 12:15:02 +0700

    eslint CRLF to LF

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   plugins: ["@typescript-eslint"],
   rules: {
-    "linebreak-style": ["error", "windows"],
+    "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
     semi: ["error", "never"],
   },
