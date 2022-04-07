7cc1b15b1785a3582e2d4ff391fa256d971959e1
Reza Ageng <rezaageng5@gmail.com>
2022-04-07 08:03:25 +0700

    update status

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,10 +3,9 @@ import { Event } from "../structures/Event"
 import http from "http"
 
 export default new Event("ready", (client) => {
-  const memberCount = client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)
-
   http.createServer((_req, res) => res.end("ily :3")).listen(3000)
 
-  client.user.setActivity(`With ${memberCount} others`, { type: "PLAYING" })
+  client.user.setStatus("idle")
+  client.user.setActivity("With You", { type: "PLAYING" })
   console.log(chalk.bgGreen.black(`${client.user.tag} is alive!`))
 })
